9bcfb2b74ce30a256a6fa05213590cb44e742790
Nicholas <nick.nogue28@outlook.com>
2025-09-21 19:16:31 -0300

    fix: add more tests

--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -25,4 +25,41 @@ func TestHeadersParse(t *testing.T) {
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
 	assert.False(t, done)
+
+	// Test: Valid single header with extra whitespace
+	headers = NewHeaders()
+	data = []byte("Host:   localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 25, n)
+	assert.False(t, done)
+
+	// Test: Valid 2 headers with existing headers
+	headers = NewHeaders()
+	data = []byte("Host:   localhost:42069\r\nHost:     localhost:42070\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	assert.Equal(t, 25, n)
+	assert.False(t, done)
+	n, done, err = headers.Parse(data[n:])
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42070", headers["Host"])
+	assert.Equal(t, 27, n)
+	assert.False(t, done)
+
+	// Test: Valid done
+	headers = NewHeaders()
+	data = []byte("Host:   localhost:42069   \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	assert.Equal(t, 28, n)
+	assert.False(t, done)
+	n, done, err = headers.Parse(data[n:])
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, 2, n)
+	assert.True(t, done)
 }
