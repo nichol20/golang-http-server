dc047b75bda96ea23e5c2612c00f83fad82b79b8
Nicholas <nick.nogue28@outlook.com>
2025-10-01 23:42:23 -0300

    refactor: change package name to main and implement server code

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,14 +1,63 @@
 package server
 
-import "net"
+import (
+	"fmt"
+	"log"
+	"net"
+	"sync/atomic"
+
+	"github.com/nichol20/http-server/internal/request"
+)
 
 type Server struct {
+	Addr     string
+	listener net.Listener
+	closed   *atomic.Bool
 }
 
-func Serve(port int) (*Server, error)
+func Serve(port uint16) (*Server, error) {
+	addr := fmt.Sprintf(":%d", port)
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		return nil, err
+	}
+	closed := &atomic.Bool{}
+	closed.Store(false)
+	s := &Server{Addr: addr, listener: listener, closed: closed}
 
-func (s *Server) Close() error
+	go s.listen()
 
-func (s *Server) listen()
+	return s, nil
+}
 
-func (s *Server) handle(conn net.Conn)
+func (s *Server) Close() error {
+	fmt.Println("server closed!")
+	s.closed.Store(false)
+	return s.listener.Close()
+}
+
+func (s *Server) listen() {
+	for !s.closed.Load() {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			log.Fatal("Error accepting connection: ", err)
+		}
+
+		go s.handle(conn)
+	}
+}
+
+func (s *Server) handle(conn net.Conn) {
+	_, err := request.RequestFromReader(conn)
+	if err != nil {
+		log.Fatal("Error parsing request: ", err)
+	}
+
+	res := "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nHello World!"
+	conn.Write([]byte(res))
+
+	err = conn.Close()
+	if err != nil {
+		log.Fatal("error closing connection: ", err)
+	}
+}
